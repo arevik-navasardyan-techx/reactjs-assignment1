85352e23e492b47afc944f87f95d65f860a33005
arevik <arevik.navasardyan.techx@gmail.com>
2025-08-12 11:19:15 +0400

    Correcting few issues

--- react-assignment1/src/App.jsx
+++ react-assignment1/src/App.jsx
@@ -1,15 +1,22 @@
 import "./App.css";
 import ProfileCard from "./components/ProfileCard/ProfileCard.jsx";
-import AliceImage from "./assets/Alice.jpg";
+// import AliceImage from "./assets/Alice.jpg";
 import BobImage from "./assets/Bob.jpeg";
 import MarkImage from "./assets/Mark.webp";
 
 function App() {
   return (
     <>
-      <ProfileCard name="Alice" title="Maneger" imageURL="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.photostudio308.com%2Fportfolio-item%2Fphoto-studio-308-professional-headshots-new-york-city-2022-ii%2F&psig=AOvVaw3Ej5TV5Yzx7fhsyrUmQQ3S&ust=1755058735659000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCPDSr7a1hI8DFQAAAAAdAAAAABAE" />
-      <ProfileCard name="Bob" title="Engineer" imageURL={BobImage} />
-      <ProfileCard name="Mark" title="Director" imageURL={MarkImage} />
+      <h1>Our Professionals</h1>
+      <div className="card-container">
+        <ProfileCard
+          name="Alice"
+          title="Maneger"
+          imageURL="https://www.whiteroomstudio.com.sg/wordpress/wp-content/uploads/2021/10/professional-headshot-photography-linkedin-singapore-2.jpg"
+        />
+        <ProfileCard name="Bob" title="Engineer" imageURL={BobImage} />
+        <ProfileCard name="Mark" title="Director" imageURL={MarkImage} />
+      </div>
     </>
   );
 }
--- react-assignment1/src/components/ProfileCard/ProfileCard.jsx
+++ react-assignment1/src/components/ProfileCard/ProfileCard.jsx
@@ -1,9 +1,9 @@
 import './ProfileCard.css' 
 import React from 'react';
 
-export default function ProfileCard({name, title, imageUrl}){
-    return <div className="card-container">
-        <img src={imageUrl} alt={name} className='img'/>
+export default function ProfileCard({name, title, imageURL}){
+    return <div className="card">
+        <img src={imageURL} alt={name} className='img'/>
         <div className="describtion">
             <p>{name}</p>
             <p>{title}</p>
